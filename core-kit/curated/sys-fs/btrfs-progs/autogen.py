d7ef17e1fc68d638c4644a9cae1afe70622d6a62
Saulius Gurklys <s4uliu5@gmail.com>
2022-05-23 09:17:02 +0300

    Fix merge

--- core-kit/curated/sys-fs/btrfs-progs/autogen.py
+++ core-kit/curated/sys-fs/btrfs-progs/autogen.py
@@ -1,36 +1,5 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
-import json
-import re
-from packaging import version
-
-def get_latest_tag(json):
-    return sorted(json, key=lambda tag: version.parse(tag["name"])).pop()
-
-
-async def generate(hub, **pkginfo):
-
-	github_user = "kdave"
-	github_repo = pkginfo["name"]
-	json_data = await hub.pkgtools.fetch.get_page(
-		f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
-	)
-
-	latest_tag = get_latest_tag(json_data)
-	if latest_tag is None:
-		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable tag of {repo}")
-
-	tag = latest_tag["name"]
-	final_name = f"{github_repo}-{tag}.tar.xz"
-	url = f"https://www.kernel.org/pub/linux/kernel/people/{github_user}/{github_repo}/{final_name}"
-
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=tag.lstrip("v"),
-		github_user=github_user,
-		github_repo=github_repo,
-=======
 from bs4 import BeautifulSoup
 from packaging import version
 
@@ -55,7 +24,6 @@ async def generate(hub, **pkginfo):
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=latest_version.lstrip("v"),
->>>>>>> master
 		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
 	)
 	ebuild.push()
