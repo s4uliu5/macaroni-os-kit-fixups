15b0d0cf7f9a18f567ff3304a9c22230d11b4f7e
Saulius Gurklys <s4uliu5+funtoo@gmail.com>
2025-10-05 21:47:00 +0300

    [release-branch][core-server-kit] dev-db/redis: autogen 8.2.x, 8.0.x, 7.4.x, 7.2.x and 6.x

--- core-server-kit/curated/dev-db/redis/autogen.py
+++ core-server-kit/curated/dev-db/redis/autogen.py
@@ -9,7 +9,7 @@ async def query_github_api(user, repo, query):
 
 
 async def generate(hub, **pkginfo):
-	majors = [ '7', '6' ]
+	majors = [ '8.2', '8.0', '7.4', '7.2', '6' ]
 	github_user = "redis"
 	github_repo = "redis"
 	json_data = await query_github_api(github_user, github_repo, "releases")
