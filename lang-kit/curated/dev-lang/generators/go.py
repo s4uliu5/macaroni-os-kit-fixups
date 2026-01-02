e28d4b2f193393f58530e4cffd196214f14cd54e
Saulius Gurklys <s4uliu5+funtoo@gmail.com>
2026-01-02 17:37:02 +0200

    [release-branch][lang-kit] dev-lang/go: one mobe url fix

--- lang-kit/curated/dev-lang/generators/go.py
+++ lang-kit/curated/dev-lang/generators/go.py
@@ -5,7 +5,7 @@ GLOBAL_DEFAULTS = {}
 
 async def generate(hub, **pkginfo):
 	artifact = hub.pkgtools.ebuild.Artifact(
-		url=f"https://storage.googleapis.com/golang/go{pkginfo['version']}.src.tar.gz"
+		url=f"https://dl.google.com/go/go{pkginfo['version']}.src.tar.gz"
 	)
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(**pkginfo, artifacts=[artifact])
 	ebuild.push()
